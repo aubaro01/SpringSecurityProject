cf6e610447540d8c59f613a7a45da33b42b61530
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2026-03-06 15:55:50 +0000

    Refactor project structure
    Refactor project structure, add new folder and logic

---
+++ spring-security/src/main/java/com/aubaro/spring_security/Core/Message/authErrorMessage.java
@@ -0,0 +1,15 @@
+package com.aubaro.spring_security.Core.Message;
+
+import org.apache.axiom.locator.Implementation;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@RequiredArgsConstructor
+@ToString
+public interface authErrorMessage Implementation ResponseEnvelopeCode.ResponseEnvelopeCodes
+{
+
+}
