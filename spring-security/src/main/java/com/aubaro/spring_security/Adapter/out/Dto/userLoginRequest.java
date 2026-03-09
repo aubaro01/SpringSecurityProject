171e34008459807a0ad43962041fbddc9791e684
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2026-03-09 11:12:55 +0000

    Build
    add message error

--- spring-security/src/main/java/com/aubaro/spring_security/Adapter/out/Dto/userLoginRequest.java
+++ spring-security/src/main/java/com/aubaro/spring_security/Adapter/out/Dto/userLoginRequest.java
@@ -1,4 +1,4 @@
-package com.aubaro.spring_security.Adapter.out.Dto;
+package com.aubaro.spring_security.Adapter.out.DTO;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -9,10 +9,10 @@ import lombok.*;
 @Setter
 public class userLoginRequest {
 
-    @NotNull(message = "O username não deve estar vazia ou null!!!")
+    @NotNull(message = "O username não deve estar vazia ")
     private String username;
 
-    @NotEmpty(message = "A password não deve estar vazia ou null!!!")
+    @NotEmpty(message = "A password não deve estar vazia")
     private String password;
 
 }
--- spring-security/src/main/java/com/aubaro/spring_security/Core/Message/MessageError.java
+++ spring-security/src/main/java/com/aubaro/spring_security/Core/Message/MessageError.java
@@ -9,7 +9,10 @@ public enum MessageError {
 
     AU0001("AU0001","Invalid credentials"),
     AU0002("AU0002","User not found"),
-    AU0003("AU0003","Access denied");
+    AU0003("AU0003","Access denied"),
+    AU0004("AU0004","User not found."),
+    AU0005("AU0005","O username não deve estar vazia"),
+    AU0006("AU0004","Password não deve estar vazia");
 
     
     private final String code;
